17a45fb16deb73646905df12139e01faadc59c00
Lilli <caothivananh98@gmail.com>
2021-10-28 16:38:09 +0200

    remove unnecessary tkit dependencies

--- src/main/java/com/devonfw/quarkus/ordermanagement/service/v1/mapper/ItemMapper.java
+++ src/main/java/com/devonfw/quarkus/ordermanagement/service/v1/mapper/ItemMapper.java
@@ -3,11 +3,9 @@ package com.devonfw.quarkus.ordermanagement.service.v1.mapper;
 import com.devonfw.quarkus.ordermanagement.domain.model.ItemEntity;
 import com.devonfw.quarkus.ordermanagement.service.v1.model.ItemDto;
 import org.mapstruct.Mapper;
-import org.tkit.quarkus.rs.mappers.OffsetDateTimeMapper;
-
 import java.util.List;
 
-@Mapper(uses = OffsetDateTimeMapper.class)
+@Mapper
 public interface ItemMapper {
     List<ItemDto> map(List<ItemEntity> items);
 }
