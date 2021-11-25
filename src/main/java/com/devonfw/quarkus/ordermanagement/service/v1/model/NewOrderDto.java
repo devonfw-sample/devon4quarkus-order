56ea39ff99741d1e638a7afae1dac77f870f40c0
Julian Günther <julian.guenther@capgemini.com>
2021-11-25 14:03:46 +0100

    added servicedocgen plugin and removed openapi-smallrye extension

--- src/main/java/com/devonfw/quarkus/ordermanagement/service/v1/model/NewOrderDto.java
+++ src/main/java/com/devonfw/quarkus/ordermanagement/service/v1/model/NewOrderDto.java
@@ -3,8 +3,6 @@ package com.devonfw.quarkus.ordermanagement.service.v1.model;
 import java.time.Instant;
 import java.util.List;
 
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,9 +10,7 @@ import lombok.Setter;
 @Setter
 public class NewOrderDto {
 
-  @Schema(nullable = false, description = "Payment date in format yyyy-mm-ddThh:mm:ssZ")
   private Instant paymentDate;
 
-  @Schema(nullable = false, description = "List ids of ordered products")
   private List<Long> orderedProductIds;
 }
